ad6e14dbf80716851b75a5cffc917df508178e8e
indigoranger158 <68606667+indigoranger158@users.noreply.github.com>
2020-08-16 13:10:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION["datos-usuario"])) {
 ?>
 <html>
     <head>
-        <title>Papi PetStore | Inicio</title>
+        <title>Happy PetStore | Inicio</title>
         <!-- for-mobile-apps -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
