cfdb2d8dd80a54e2a10ab213f698295cf4b93b36
indigoranger158 <68606667+indigoranger158@users.noreply.github.com>
2020-08-16 13:31:25 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,21 @@
 <?php
 /*Conexion con el servidor*/
-$hostname = "MYSQLCONNSTR_localdb"; // servidor local, si fuera externo se pone la ip
-$database = "bd_proyecto_tienda";
-$username = "azure";
-$passwordbd = "";
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
 
-$mysql = new mysqli($hostname, $username, $passwordbd, $database);
+$mysql = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
 if ($mysql->connect_errno) {
     $error_conexion = "Fallo la conexión";
