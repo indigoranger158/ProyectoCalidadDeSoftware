eebc3e8e8f22fa241101337dec308c79a5578b10
indigoranger158 <68606667+indigoranger158@users.noreply.github.com>
2020-08-16 12:25:35 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 /*Conexion con el servidor*/
-$hostname = "52.173.28.95"; // servidor local, si fuera externo se pone la ip
+$hostname = "localhost"; // servidor local, si fuera externo se pone la ip
 $database = "bd_proyecto_tienda";
 $username = "root";
 $passwordbd = "";
