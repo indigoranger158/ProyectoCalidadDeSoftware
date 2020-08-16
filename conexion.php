2defc61ccd0d12b8727fead85441832417f17292
indigoranger158 <68606667+indigoranger158@users.noreply.github.com>
2020-08-16 13:03:34 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 /*Conexion con el servidor*/
-$hostname = "52.173.28.95"; // servidor local, si fuera externo se pone la ip
+$hostname = "MYSQLCONNSTR_localdb"; // servidor local, si fuera externo se pone la ip
 $database = "bd_proyecto_tienda";
 $username = "azure";
 $passwordbd = "";
