7f2fa30f2dace3e9ebdb11434f8b2f5308dfff05
indigoranger158 <68606667+indigoranger158@users.noreply.github.com>
2020-08-16 13:36:07 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,21 +1,12 @@
 <?php
 /*Conexion con el servidor*/
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
+$hostname = "localhost"; // servidor local, si fuera externo se pone la ip
+$database = "bd_proyecto_tienda";
+$username = "root";
+$passwordbd = "";
 
-$mysql = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+$mysql = new mysqli($hostname, $username, $passwordbd, $database);
 
 if ($mysql->connect_errno) {
     $error_conexion = "Fallo la conexión";
