6f2e2d0b71ba29dad7745593bcf8d335032e28c2
indigoranger158 <68606667+indigoranger158@users.noreply.github.com>
2020-08-21 22:27:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION["datos-usuario"])) {
 ?>
 <html>
     <head>
-        <title>Firulais Fidelitas Costa Rica Pet-Store: sus prendas para mascota</title>
+        <title>Happy Petshop: La tienda para sus mascota</title>
         <!-- for-mobile-apps -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
