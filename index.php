87789addba1392d56e7df6318b029dc017bea531
NamaLuis <67727396+NamaLuis@users.noreply.github.com>
2020-08-21 18:54:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION["datos-usuario"])) {
 ?>
 <html>
     <head>
-        <title>Pet Store: La mejor Tienda de Ropa para Animales</title>
+        <title>Firulais Store: La mejor Tienda de Ropa para Animales</title>
         <!-- for-mobile-apps -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
