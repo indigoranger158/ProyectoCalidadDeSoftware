663dc5cef1053dc164170856c31bb95cb22839cf
Justin-stack <68601657+Justin-stack@users.noreply.github.com>
2020-08-25 09:43:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION["datos-usuario"])) {
 ?>
 <html>
     <head>
-        <title>Happy PetShop</title>
+        <title>Proyecto</title>
         <!-- for-mobile-apps -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
